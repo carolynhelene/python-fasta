ba4b7b4cbce56e86df016f4bcb33624e774b80b1
Carolyn Baloh <carolyn.baloh@duke.edu>
2019-11-05 09:49:42 -0500

    Initial version of fasta reader

---
+++ python-fasta.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def read_fasta(filename):
+    sequence = ''
+    f = open(filename)
+    for line in f:
+        line = line.strip()
+        if not '>' in line:
+            # Append to the last sequence
+            sequence = sequence + line
+    f.close()
+    return sequence
+
+print(read_fasta('ae.fa'))
+
+
+# In[ ]:
+
+
+
+
